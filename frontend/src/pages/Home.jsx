e6ff28a466639ee4e7707162251e0f012af09d45
Sandy <93102462+sandycdutra@users.noreply.github.com>
2025-05-07 12:51:37 -0300

    Project architecture

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import logoJukebox from '../assets/jukebox_logo.png';
+
+function Home() {
+  return (
+    <div>
+      <img src={logoJukebox} alt="logo" />
+    </div>
+  )
+}
+
+export default Home
