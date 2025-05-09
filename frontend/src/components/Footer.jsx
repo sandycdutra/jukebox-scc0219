146b30539afcbfd599d24ccd576d2ca4dd515389
Sandy <93102462+sandycdutra@users.noreply.github.com>
2025-05-09 10:18:20 -0300

    Add icons to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,25 @@
 import "../css/footer.css";
 import "../css/main.css";
+import instagramLogo from "../assets/icons/logo-instagram.png";
+import tiktokLogo from "../assets/icons/logo-tiktok.png";
+import twitterLogo from "../assets/icons/logo-twitter.png";
+import mastercardLogo from "../assets/icons/logo-mastercard.png";
+import visaLogo from "../assets/icons/logo-visa.png";
+import pixLogo from "../assets/icons/logo-pix.png";
+
 
 function Footer () {
     return (
         <footer className="footer">
         <div className="footer-content">
-            <div className="footer-section">
+            <div className="footer-section-text">
                 <h4>JUKEBOX</h4>
                 <a href="#">Home</a>
                 <a href="#">Contact us</a>
                 <a href="#">Terms and conditions</a>
                 <a href="#">Privacy politics</a>
             </div>
-            <div className="footer-section">
+            <div className="footer-section-text">
                 <div className="footer-categories">
                     <h4>CATEGORIES</h4>
                     <a href="#">Accessories</a>
@@ -29,20 +36,20 @@ function Footer () {
                     <a href="#">Rock</a>
                 </div>
             </div>
-            <div className="footer-section">
+            <div className="footer-section-logo">
                 <h4>FOLLOW US</h4>
                 <div className="social-icons">
-                  <span>📸</span>
-                  <span>🎵</span>
-                  <span>🐦</span>
+                    <a href="https://www.instagram.com" target="_blank"><img src={instagramLogo}  alt="Instagram" /></a>
+                    <a href="https://www.tiktok.com/" target="_blank"><img src={tiktokLogo}  alt="TikTok" /></a>
+                    <a href="https://x.com/" target="_blank"><img src={twitterLogo}  alt="Instagram" /></a>
                 </div>
             </div>
-            <div className="footer-section">
+            <div className="footer-section-logo">
                 <h4>PAYMENT</h4>
                 <div className="payment-icons">
-                    <span>💳</span>
-                    <span>💳</span>
-                    <span>💳</span>
+                    <a href="https://www.mastercard.com.br/pt-br.html" target="_blank"><img src={mastercardLogo}  alt="Mastercard" /></a>
+                    <a href="https://www.visa.com.br/" target="_blank"><img src={visaLogo}  alt="Visa" /></a>
+                    <a href="https://www.bcb.gov.br/estabilidadefinanceira/pix" target="_blank"><img src={pixLogo}  alt="Pix" /></a>
                 </div>
             </div>
         </div>
