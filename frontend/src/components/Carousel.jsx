726c3ea7867c2a1ece953376e8b9f542763c2de8
Sandy <93102462+sandycdutra@users.noreply.github.com>
2025-06-16 18:04:21 -0300

    Fixing carousel responsiveness

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules'; // Adicione Autoplay aqui
 import { Link } from 'react-router-dom';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -12,21 +12,32 @@ function Carousel() {
 
     return (
         <section className="carousel-section">
-        <Swiper
-            modules={[Navigation, Pagination]}
-            loop={true}
-            pagination={{ clickable: true }}
-            // navigation={true} // setas com erro
-            className="Swiper"
-        >
-            {banners.map((banner, index) => (
-                <SwiperSlide key={index}>
-                    <Link to={`/product/${banner.id}`} className="carousel-link">
-                        <img src={banner.image} alt={banner.alt} />
-                    </Link>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+            <Swiper
+                modules={[Autoplay, Navigation, Pagination]}
+                loop={true}
+                pagination={{ clickable: true }}
+                // navigation={true} //setas com erro
+                autoplay={{ // Adicionado para que o carrossel avance automaticamente
+                    delay: 5000, // 5 segundos
+                    disableOnInteraction: false, // Continua o autoplay mesmo após interação do usuário
+                }}
+                className="Swiper" // Classe aplicada ao container Swiper
+                // --- PROPRIEDADES DE RESPONSIVIDADE PARA O SWIPER ---
+                slidesPerView={1} // Por padrão, sempre 1 slide visível (ótimo para mobile)
+                spaceBetween={0} // Sem espaço entre os slides
+            >
+                {banners.map((banner, index) => (
+                    <SwiperSlide key={index}>
+                        <Link to={`/product/${banner.id}`} className="carousel-link">
+                            <img 
+                                src={banner.image} 
+                                alt={banner.alt} 
+                                style={{ width: '100%', height: 'auto', display: 'block' }} 
+                            />
+                        </Link>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </section>
     )
 }
